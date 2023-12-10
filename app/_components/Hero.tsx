170ea806d2884619aed9e41c3eabc49f7f572b18
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-10 17:27:57 +0500

    'commit-1'

---
+++ app/_components/Hero.tsx
@@ -0,0 +1,38 @@
+export default function Hero () {
+  return (
+    <>
+      <section className='bg-gray-50'>
+        <div className='mx-auto max-w-screen-xl px-4 py-32 lg:flex '>
+          <div className='mx-auto max-w-xl text-center'>
+            <h1 className='text-3xl font-extrabold sm:text-5xl'>
+              <span className='text-primary'>Upload, Save</span> and easily{' '}
+              <span className='text-primary'>share</span> your files in one
+              place
+            </h1>
+
+            <p className='mt-4 sm:text-xl/relaxed text-gray-500'>
+              Drag and drop your files directly on our cloud and share your
+              friends securely with password and send it on email as well.
+            </p>
+
+            <div className='mt-8 flex flex-wrap justify-center gap-4'>
+              <a
+                className='block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto'
+                href='/get-started'
+              >
+                Get Started
+              </a>
+
+              <a
+                className='block w-full rounded px-12 py-3 text-sm font-medium text-blue shadow hover:text-blue-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto'
+                href='/about'
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
